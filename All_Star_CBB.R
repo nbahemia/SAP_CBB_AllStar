be84ad48c52bc067a2c4ce5d61683ecd3d7d19e7
Naeem Bahemia <naeembahemia@gmail.com>
2025-11-02 13:15:54 -0500

    Add initial All-Star CBB analysis script and data
    Added All_Star_CBB.R script to load and preview college basketball player and drafted player data from 2009-2021. Included the corresponding Excel data files for analysis.

---
+++ All_Star_CBB.R
@@ -0,0 +1,11 @@
+# All-Star College Basketball Player Project
+# Years covered, 2009 - 2021.
+
+library(readxl)
+
+CBBPlayers <- read_excel("C:/Users/naeem/OneDrive/Desktop/Code/Personal/SAP All-Star/CollegeBasketballPlayers2009-2021.xlsx")
+DraftedPlayers <- read_excel("C:/Users/naeem/OneDrive/Desktop/Code/Personal/SAP All-Star/DraftedPlayers2009-2021.xlsx")
+
+head(CBBPlayers)
+head(DraftedPlayers)
+
